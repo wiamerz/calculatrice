28893f838a72ef50d54d2d4a0ca6eee6b6922ebf
waime <waime@gmail.com>
2024-12-11 11:44:59 +0100

    menu

---
+++ Calculatrice.js
@@ -0,0 +1,17 @@
+const prompt = require ('prompt-sync')();
+console.log("~~~~~~~~~~Menu~~~~~~~~~~~~~~~");
+console.log("1.addition");
+console.log("2.soustraction");
+console.log("3.multiplication");
+console.log("4.division");
+console.log("5.puisance");
+console.log("6.racine carree");
+console.log("7.factorielle");
+
+const num1 = prompt("entrer le premier nombre:");
+
+
+
+  
+
+  
