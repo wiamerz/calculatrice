060195f778aa2c4ab38ac5195cda9f5134d936c9
waime <waime@gmail.com>
2024-12-12 11:26:54 +0100

    sqrt,fact

--- Calculatrice.js
+++ Calculatrice.js
@@ -1,51 +1,74 @@
-const prompt = require ('prompt-sync')();
+const prompt = require('prompt-sync')();
 
-const op = prompt("Entrez l'opération (+, -, *, /, ^, √, ! ):");
-const num1 = Number(prompt("entrer le premier nombre:"));
-const num2 = Number(prompt("entrer le deuxieme nombre:"));
-
-switch(op){ 
+while (true) {
+  const op = prompt("Entrez l'opération (+, -, *, /, ^, <, ! ) ou ( q ) pour quitter : ");
   
-  case '+':
-     
-     const sum = num1 + num2;
-     console.log(`le resultat est : ${sum}`);
-    break;
-  
-  case '-':
-    
-     sous = num1 - num2;
-    console.log(`le resultat est : ${sous}`);
-    break;
+  if (op == 'q') {
+      console.log("Merci d'avoir utilisé notre calculatrice. Au revoir ");
+      break;
+  }
 
-  case '*':
-    mult = num1 * num2;
-    console.log(`le resultat est : ${mult}`);
-    break;
+  const num1 = Number(prompt("entrer le nombre:"));
 
-  case '/':
-    if (num2 == 0){
-      console.log("erreur ne divis pas par 0");
-    }else {
-    div = num1 / num2;
-    console.log(`le resultat est : ${div}`);
-    }
-    break;
-  
-   case '^':
+switch(op) {
+    case '+':
+        const num2_add = Number(prompt("ajoute le deuxieme nombre:"));
+        const sum = num1 + num2_add;
+        console.log(`le resultat est : ${sum}`);
+        break;
 
+    case '-':
+        const num2_sous = Number(prompt("ajoute le deuxieme nombre:"));
+        const sous = num1 - num2_sous;
+        console.log(`le resultat est : ${sous}`);
+        break;
 
-    break;
-  
-  case '√':
-    break;
- 
-  case '!':
-    break;
-    */
+    case '*':
+        const num2_mult = Number(prompt("ajoute le deuxieme nombre:"));
+        const mult = num1 * num2_mult;
+        console.log(`le resultat est : ${mult}`);
+        break;
 
-  default :
-    console.log("erreur");
-    resultat = undefined;
-  
+    case '/':
+        const num2_div = Number(prompt("ajoute le deuxieme nombre:"));
+        if (num2_div === 0) {
+            console.log("erreur ne divise pas par 0");
+        } else {
+            const div = num1 / num2_div;
+            console.log(`le resultat est : ${div}`);
+        }
+        break;
+
+    case '^':
+        const num2_puiss = Number(prompt("ajoute le deuxieme nombre:"));
+        const puiss = Math.pow(num1, num2_puiss);
+        console.log(`le resultat est : ${puiss}`);
+        break;
+
+    case '<':
+        if (num1<0){
+            console.log("error, the number should note be negative ");
+        } else { 
+            sqrt = Math.sqrt(num1);
+            console.log(`le resultat est : ${sqrt}`);
+        }
+        break;
+
+    case '!':
+        if (num1 < 0) {
+            console.log(`La factorielle ne peut pas être négative`);
+        } else if (num1 === 0 || num1 === 1) {
+            console.log(`Le resultat est : 1`);
+        } else {
+            let resultat = 1;
+            for (let i = 2; i <= num1; i++) {
+                resultat *= i;
+            }
+            console.log(`Le resultat est : ${resultat}`);
+        }
+        break;
+
+    default:
+        console.log(`Paramètre incorrect`);
+}
 }
